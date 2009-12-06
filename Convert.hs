d844811623f27dff1cf27202b0cc1c331ffbee44
Simon Brenner <olsner@gmail.com>
2009-12-06 03:10:36 +0100

    More strictness.

--- Convert.hs
+++ Convert.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TypeOperators,BangPatterns #-}
+{-# OPTIONS_GHC -funbox-strict-fields #-}
+
 {-
 
 Pre/Post-processing of mallog data. The main objective of this step is to
@@ -38,42 +41,47 @@ main = do
     res `seq` hPrintf stderr "Conversion done! %d operations.\n" (length res)
     B.putStr (output res)
 
+data a :*: b = !a :*: !b
 data Action a = Alloc !a !a | Free !a
 type Size = Word64
 type Ptr = Word64
 
-parseIn :: [(Word64,Word64)] -> [Action Word64]
-parseIn = map (\(s,p) -> if s == 0 then Free p else Alloc (s `div` 2) p)
+instance (NFData a, NFData b) => NFData (a :*: b) where
+    rnf (a :*: b) = rnf a `seq` rnf b
 
-process :: (Eq a, Ord a, Num a, NFData a) => [Action a] -> [(Word64,a)]
-process = execWriter . foldM f (-1,[],M.empty)
+parseIn :: [Word64 :*: Word64] -> [Action Word64]
+parseIn = map (\(s :*: p) -> if s == 0 then Free p else Alloc (s `div` 2) p)
+
+process :: (Eq a, Ord a, Num a, NFData a) => [Action a] -> [Word64 :*: a]
+process = execWriter . go 0 [] M.empty
   where
-    f (max,  fs,m) (Free p)    =
+    go !max !fs     !m (Free !p:xs)    =
         case M.lookup p m of
-            Just f -> tell [(f,0)] >> return (max, f:fs, M.delete p m)
+            Just f -> tell [f :*: 0] >> go max (f:fs) (M.delete p m) xs
             -- If we free an unknown pointer, just ignore the operation.
-            Nothing -> return (max,fs,m)
-    f (max,f:fs,m) (Alloc s p) = tell [(f,s)] >> return (max, fs, M.insert p f m)
-    f (max,  [],m) (Alloc s p) = tell [(f,s)] >> return (f,   [], M.insert p f m) where f = max + 1
+            Nothing -> go max fs m xs
+    --f (max,  [],m) (Alloc s p) = tell [(f,s)] >> return (f,   [], M.insert p f m) where f = max + 1
+    go max []     m (Alloc s p:xs) = tell [max :*: s] >> go (max+1) [] m xs
+    go max (f:fs) m (Alloc s p:xs) = tell [f   :*: s] >> go max     fs (M.insert p f m) xs
 
 type ByteString = B.ByteString
 
-maximumFst = maximum . map fst
+maximumFst = maximum . map (\(a :*: b) -> a)
 
 toWord32 x = fromIntegral x :: Word32
 
-output :: [(Word64,Word64)] -> ByteString
+output :: [Word64 :*: Word64] -> ByteString
 output xs = runPut $ do
     let max = maximumFst xs
     putWord64le . fromIntegral $ max
-    let xs' = map (toWord32 *** toWord32) xs
+    let xs' = map (\(a :*: b) -> toWord32 a :*: toWord32 b) xs
     if max >= 2^32
-        then mapM_ (\(i,a) -> putWord64le i >> putWord64le a) xs
-        else mapM_ (\(i,a) -> putWord32le i >> putWord32le a) xs'
+        then mapM_ (\(i :*: a) -> putWord64le i >> putWord64le a) xs
+        else mapM_ (\(i :*: a) -> putWord32le i >> putWord32le a) xs'
 
-getAll :: Binary a => Get a -> ByteString -> [a]
+getAll :: Get a -> ByteString -> [a]
 getAll get = runGet getter
   where
     getter = isEmpty >>= \b -> if b then return [] else liftM2 (:) get getter
 
-input = getAll (liftM2 (,) getWord64le getWord64le)
+input = getAll (liftM2 (:*:) getWord64le getWord64le)
